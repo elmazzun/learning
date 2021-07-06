377fcb9770c5e4e6665a13ea3bbd53b56eb8ccf3
elmazzun <mazzocchiandrea24@gmail.com>
2021-07-06 19:37:21 +0200

    refactor: fix Binary Search Tree

--- challenges/amazon/BstNode.js
+++ challenges/amazon/BstNode.js
@@ -1,4 +1,5 @@
 'use strict';
+
 module.exports = class BstNode {
   constructor(value, leftChild = null, rightChild = null) {
     this.value = value;
--- challenges/amazon/SingleLinkedList.js
+++ challenges/amazon/SingleLinkedList.js
@@ -1,8 +1,51 @@
 "use strict";
 const Node = require('./SingleLinkedListNode.js');
 
+// [head] -> [x|next] -> [y|next] -> null
+
 module.exports = class SingleLinkedList {
   constructor() {
     this.head = null;
+  }
+  insertAtHead(newData) {
+    let newNode = new Node(newData);
+    newNode.next = this.head;
+    this.head = newNode;
+    return this; // return updated list
+  }
+  isEmpty() {
+    return this.head == null;
+  }
+  printList() {
+    if (this.isEmpty()) {
+      return false;
+    }
+    let tmp = this.head;
+    while (tmp) {
+      console.log(tmp.val);
+      tmp = tmp.next;
+    }
+    console.log('null');
+    return true;
+  }
+  getHead() {
+    return this.head;
+  }
+  setHead(newHead) {
+    this.head = newHead;
+    return this;
+  }
+  insertAtTail(newData) {
+    const newNode = new Node();
+    if (this.isEmpty()) {
+      
+    }
+
+    let tmp = this.head;
+    while (tmp.next != null) {
+      tmp = tmp.next;
+    }
+    tmp.next = newNode;
+    return this;
   }
 }
